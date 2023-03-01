2dce3dabd52a7ea6489638141834edc17e56f21f
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-03-01 14:57:44 -0300

    finalizando modal

---
+++ src/components/PhotoElements/ModalPhoto.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { baseUrlApi } from '../../utils/baseUrlApi'
+import { photo } from '../../utils/types/ElementsInterface'
+import { Button } from '../FormElements'
+import { AiFillCloseCircle, AiFillEye } from 'react-icons/ai'
+
+interface ModalPhotoProps {
+  setPhotoCurrent: React.Dispatch<React.SetStateAction<photo | null>>
+  photo: photo
+}
+
+const ModalPhoto = ({ setPhotoCurrent, photo }: ModalPhotoProps) => {
+  return (
+    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[540px] z-10 bg-gray-100 rounded-lg flex flex-col p-2 gap-2'>
+
+      <button className='text-6xl absolute ml-[500px] mt-[-40px] bg-teal-500 rounded-full hover:text-white'
+      onClick={() => setPhotoCurrent(null)}>
+        <AiFillCloseCircle />
+      </button>
+
+      <div className='flex justify-between items-center'>
+        <span className='flex items-center gap-2'>
+          <img src={`${baseUrlApi}${photo.User.banner}`}
+            className="w-[100px] h-[100px] rounded-full" />
+          <h2 className='text-2xl font-bold'>
+            {photo.User.name}
+          </h2>
+        </span>
+
+        <Button stylish='w-[140px] rounded-xl bg-gradient-to-tr from-yellow-500 via-amber-500 to-yellow-700'>
+          Doar
+        </Button>
+      </div>
+      <img src={`${baseUrlApi}${photo.photo}`}
+        className="h-[320px] w-full" />
+      <div className='flex flex-col gap-4'>
+        <span className='text-center font-semibold'>
+          {photo.description}
+        </span>
+        <span className='flex justify-between font-bold'>
+          <p className='text-blue-500 drop-shadow-[1px_1px_0px_black] uppercase underline decoration-yellow-500 text-2xl'>
+            Categoria: {photo.Category.title}
+          </p>
+          <p className='flex'>
+            <AiFillEye className='text-3xl' />
+            {photo.views}
+          </p>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default ModalPhoto
--- src/components/PhotoElements/PhotoContainer.tsx
+++ src/components/PhotoElements/PhotoContainer.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { baseUrlApi } from '../../utils/baseUrlApi'
 import { photo } from '../../utils/types/ElementsInterface'
 import { AiFillEye } from 'react-icons/ai'
+import ModalPhoto from './ModalPhoto'
 
-interface PhotoContainerProps{
+interface PhotoContainerProps {
   photos: photo[]
 }
 
-const PhotoContainer = ({photos}: PhotoContainerProps) => {
+const PhotoContainer = ({ photos }: PhotoContainerProps) => {
+
+  const [photoCurrent, setPhotoCurrent] = useState<photo | null>(null)
+
   return (
     <div className='grid md:grid md:grid-cols-2 lg:grid-cols-4 gap-4 mt-12 p-4 w-full justify-center items-center'>
+
+      {photoCurrent && (
+        <ModalPhoto setPhotoCurrent={setPhotoCurrent}
+        photo={photoCurrent} />
+      )}
+
       {photos.map(photo => (
-        <div className='cursor-pointer'>
+        <button className='cursor-pointer'
+          onClick={() => setPhotoCurrent(photo)}>
           <img src={`${baseUrlApi}${photo.photo}`}
-          className="rounded-lg" />
+            className="rounded-lg" />
           <span className='absolute ml-[420px] mt-[-3%] md:ml-[20%] flex font-bold'>
             <AiFillEye className='md:text-4xl text-4xl' />
             {photo.views}
           </span>
-        </div>
+        </button>
       ))}
     </div>
   )
